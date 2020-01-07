18134d267c95847321388e329ff6e02351ea4ba0
Tito <anton.chernov@vimmi.net>
2020-01-07 20:20:06 +0200

    For tomorrow

--- nest.py
+++ nest.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import argparse
-import json
 import os
 import logging
+
+from pandas import read_json
 
 logging.basicConfig(
     format='%(asctime)s %(message)s',
@@ -15,13 +16,22 @@ class DataHandler(object):
             self,
             file_path=None,
             currency=None,
-            country=None,
-            city=None):
-        self.currency = currency
-        self.country = country
-        self.city = city
+            # country=None,
+            # city=None
+    ):
+        self.currency = self.check_currency(currency)
+        # self.country = country
+        # self.city = city
         self.path_to_file = self.file_exist(file_path)
         logging.info('Path: {0}'.format(self.path_to_file))
+
+    def check_currency(self, cur):
+        if isinstance(cur, str):
+            return cur.split(',')
+        elif isinstance(cur, list):
+            return cur
+        else:
+            raise TypeError('Unrecognized type of currency!')
 
     def file_exist(self, path):
         _ = path if path else ''.join([os.path.curdir, '/data.json'])
@@ -36,15 +46,36 @@ class DataHandler(object):
 
     def data_file_reader(self):
         with open(self.path_to_file, 'r') as f:
-            logging.debug(f)
-            logging.debug(f.readlines())
-            result = json.loads(f.readlines())
+            readed_obj = f.read()
+        # logging.debug('Read data from file: {0}'.format(readed_obj))
+        return readed_obj
 
-        logging.debug('Result: {0}'.format(result))
-        return result
+    def data_file_reader_pandas(self):
+        self.df = read_json(self.path_to_file, orient='records')
+        logging.debug('DF: {0}'.format(self.df))
 
-    def data_parser(self):
-        raise NotImplemented
+
+
+    def result_builder(self):
+        res = {}
+        for cur in self.currency:
+            d = self.aggregated_data[self.aggregated_data['currency'] == cur]
+
+
+    def data_aggregator(self):
+        r = self.df[
+            (self.df['country'].isin(['UK'])) &
+            (self.df['currency'].isin(['GBP', 'FBP'])) &
+            (self.df['city'].isin(['London']))
+        ]
+        logging.debug(r)
+        logging.debug(r.to_dict())
+
+    def correct_data_aggregator(self):
+        self.aggregated_data = self.df[self.df['currency'].isin(self.currency)]
+        logging.debug(self.aggregated_data)
+
+    
 
     def vars(self):
         logging.info('Variables: {0}'.format(
@@ -63,19 +94,19 @@ if __name__ == "__main__":
         action='store',
         help='Path to "JSON" file.')
 
-    input_args.add_argument(
-        '--country',
-        '-c',
-        dest='country',
-        action='store',
-        help='Country name or names separated by "," ')
-
-    input_args.add_argument(
-        '--city',
-        '-ci',
-        dest='city',
-        action='store',
-        help='City name or names separated by "," ')
+    # input_args.add_argument(
+    #     '--country',
+    #     '-c',
+    #     dest='country',
+    #     action='store',
+    #     help='Country name or names separated by "," ')
+    #
+    # input_args.add_argument(
+    #     '--city',
+    #     '-ci',
+    #     dest='city',
+    #     action='store',
+    #     help='City name or names separated by "," ')
 
     input_args.add_argument(
         '--currency',
@@ -91,9 +122,13 @@ if __name__ == "__main__":
     res = DataHandler(
         file_path=args.file_path,
         currency=args.currency,
-        country=args.country,
-        city=args.city)
-    res.data_file_reader()
+        # country=args.country,
+        # city=args.city
+    )
+    # res.data_file_reader()
+    res.data_file_reader_pandas()
+    # res.data_aggregator()
+    res.correct_data_aggregator()
 
 
 
