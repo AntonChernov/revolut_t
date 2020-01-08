dd91a7da8257c33caa4fbfb9cd91c3a38be31d4d
Anton Chernov2 <anton_chernov2@epuavinw0183.kyiv.epam.com>
2020-01-08 17:27:39 +0200

    Add api finish with CLI need add tests

--- revolut_test/revolut_test/api.py
+++ revolut_test/revolut_test/api.py
@@ -1,1 +1,37 @@
 # -*- coding: utf-8 -*-
+import json
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from nest import DataHandler
+from revolut_test.serializers import NestedListSerializer, NestedDataSerializer
+
+
+class NestedView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = [JSONRenderer]
+
+    def post(self, request, *args, **kwargs):
+        print(type(request.data))
+        data_process = DataHandler(
+            file_path=request.data,
+            currency=self.request.query_params.get('currency')
+        )
+
+        result = data_process.result_builder(
+            json_format=True
+        )
+        return Response(json.dumps(result))
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- revolut_test/revolut_test/serializers.py
+++ revolut_test/revolut_test/serializers.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+from rest_framework import serializers
+
+
+class NestedDataSerializer(serializers.Serializer):
+
+    country = serializers.CharField(allow_null=False, required=True),
+    city = serializers.CharField(allow_null=False, required=True),
+    currency = serializers.CharField(allow_null=False, required=True),
+    amount = serializers.FloatField(allow_null=False, required=True),
+
+    def validate_amount(self, value):
+        """
+        Validate amount field
+        """
+        if isinstance(value['amount'], int):
+            return float(value['amount'])
+        return value['amount']
+
+
+class NestedListSerializer(serializers.Serializer):
+
+    data_list = NestedDataSerializer(many=True)
